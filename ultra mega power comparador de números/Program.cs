d7917dfa461c51c67735feb4dce6d0a5fc3a3edd
Murilo Alves Zapiello <54750759805@info.local>
2023-04-03 15:10:25 -0300

    aula dia 03/04

---
+++ ultra mega power comparador de números/Program.cs
@@ -0,0 +1,38 @@
+﻿// Faça um programa que leia três números e mostre o maior e o menor deles.
+
+// algorítmo
+// entrada: número qualquer
+// procedimento: comparar os números para saber qual o maior e qual é o menos 
+
+Console.WriteLine($"Digite o 1° número: ");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite o 2° número: ");
+int n2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite o 3° número: ");
+int n3 = int.Parse(Console.ReadLine());
+
+if ((n1 > n2) && (n1 < n3)) {
+    Console.WriteLine($"Maior número: {n3}");
+    Console.WriteLine($"Menor número: {n2}");
+}
+else if ((n2 > n1) && (n2 < n3)) {
+    Console.WriteLine($"Maior número: {n3}");
+    Console.WriteLine($"Menos número: {n1}");
+}
+else if ((n1 > n2) && (n3 < n2)) {
+    Console.WriteLine($"Maior número: {n1}");
+    Console.WriteLine($"Menos número: {n3}");
+}
+else if ((n1 < n2) && (n3 < n1)) {
+    Console.WriteLine($"Maior número: {n2}");
+    Console.WriteLine($"Menos número: {n3}");
+}
+
+
+
+
+
+
+
---
+++ verificador de datas corretas/Program.cs
@@ -0,0 +1,33 @@
+﻿// Escreva um programa que pergunte o dia, mês e ano do aniversário de uma pessoa e diga se a data é válida ou não. Caso não seja, diga o motivo. Suponha que todos os meses tem 31 dias e que estejamos no ano de 2013.
+
+
+// algoritmo 
+// entrada: dia, mês e ano de aniversário do usuário
+// procedimento: comparar a data colocada com a data exigida
+
+Console.WriteLine($"Digite o DIA do seu aniversário: ");
+int diaAniversario = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite o MÊS do seu aniversário: ");
+int mesAniversario = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite o ANO do seu aniversário: ");
+int anoAniversário = int.Parse(Console.ReadLine());
+
+if (diaAniversario > 31){
+    Console.WriteLine($"INVÁLIDO");
+}
+else if (mesAniversario > 12) {
+    Console.WriteLine($"INVÁLIDO");
+}
+else if (anoAniversário > 2013) {
+    Console.WriteLine($"INVÁLIDO");
+}
+else {
+    Console.WriteLine($"VÁLIDO");
+}
+
+
+
+
+
